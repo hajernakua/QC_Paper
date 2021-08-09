3b88168a40e973e04bf42b30222f493c42f618fd
hajernakua <43069812+hajernakua@users.noreply.github.com>
2021-08-09 13:54:56 -0400

    Update POND_between_group.R

--- posthoc_analysis/POND_between_group.R
+++ posthoc_analysis/POND_between_group.R
@@ -1,5 +1,8 @@
 ### this script will perform the post-hoc between-diagnoses analysis examining whether specific brain ROIs that have been reported to differ between diagnosis groups also differ in the POND sample and whether the results change depending on the QC approach implemented.
 
+##loading any packages
+library(sjstats)
+
 #1. Merging the data frames of the INCLUDED sample from each QC approach and the brain metrics 
 StandardVisual_AllPOND_Brain <- merge(StandardVisual_AllPOND, BrainMeasures_AllPOND, by = "ID")
 StringentVisual_AllPOND_Brain <- merge(StringentVisual_AllPOND, BrainMeasures_AllPOND, by = "ID")
@@ -92,7 +95,7 @@ POND_Auto_Brain_ASD_ADHD_OCD <- rbind(POND_Auto_Brain_ASD, POND_Auto_Brain_ADHD,
 
 #3. Performing the group-wise analysis - will be doing this analysis for each QC approach independently 
 
-#1. Standard visual QC
+# Standard visual QC
 HippoVol_StandVis_diag <- lm(Lhippo ~ clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_StandVis_Brain_ASD_ADHD_OCD, na.action = na.omit)
 HippoVol_StandVis_diag_An <- Anova(HippoVol_LenVis_diag)
 
@@ -113,7 +116,7 @@ lOFC_StandVis_diag_An <- Anova(lOFC_StandVis_diag)
 RlOFC_StandVis_diag <- lm(R_lateralorbitofrontal_thickavg ~ clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_StandVis_Brain_ASD_ADHD_OCD, na.action = na.omit)
 RlOFC_StandVis_diag_An <- Anova(RlOFC_StandVis_diag)
 
-#2. Standard visual+ metric QC
+# Standard visual+ metric QC
 HippoVol_StandVisMet_diag <- lm(Lhippo ~ clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_StandVisMet_Brain_ASD_ADHD_OCD, na.action = na.omit)
 HippoVol_StandVisMet_diag_An <- Anova(HippoVol_StandVisMet_diag)
 
@@ -134,7 +137,7 @@ RlOFC_StandVisMet_diag <- lm(R_lateralorbitofrontal_thickavg ~ clin_diagnosis + 
 RlOFC_StandVisMet_diag_An <- Anova(RlOFC_StandVisMet_diag)
 
 
-#3. stringent visual QC
+# stringent visual QC
 HippoVol_StringVis_diag <- lm(Lhippo ~ clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_StringVis_Brain_ASD_ADHD_OCD, na.action = na.omit)
 HippoVol_StringVis_diag_An <- Anova(HippoVol_StringVis_diag)
 HippoVol_StringVis_diag_An
@@ -171,7 +174,7 @@ cohens_f(RlOFC_StringVis_diag)
 ###no significant difference here
 
 
-#4. stringent visual + metric QC
+# stringent visual + metric QC
 HippoVol_StringVisMet_diag <- lm(Lhippo ~ clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_StringVisMet_Brain_ASD_ADHD_OCD, na.action = na.omit)
 HippoVol_StringVisMet_diag_An <- Anova(HippoVol_StringVisMet_diag)
 HippoVol_StringVisMet_diag_An
@@ -200,7 +203,7 @@ summary(RlOFC_StringVisMet_diag)
 ###no significant difference here
 
 
-#5. automated QC
+# automated QC
 HippoVol_Auto_diag <- lm(Lhippo ~ clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_Auto_Brain_ASD_ADHD_OCD, na.action = na.omit)
 HippoVol_Auto_diag_An <- Anova(HippoVol_Auto_diag)
 HippoVol_Auto_diag_An
@@ -237,14 +240,85 @@ lOFC_Auto_diag_An
 summary(lOFC_Auto_diag_An)
 cohens_f(lOFC_Auto_diag_An)
 
+#### setting up interaction between QC group and diagnosis analysis
+
+#4. Setting up the approach variable for each of the QC data frames
+POND_StandVis_Brain_ASD_ADHD_OCD$Approach <- "Standard Visual"
+POND_StandVisMet_Brain_ASD_ADHD_OCD$Approach <- "Standard Visual + Metric"
+POND_StringVis_Brain_ASD_ADHD_OCD$Approach <- "Stringent Visual"
+POND_StringVisMet_Brain_ASD_ADHD_OCD$Approach <- "Stringent Visual + Metric"
+POND_Auto_Brain_ASD_ADHD_OCD$Approach <- "Automated"
+
+POND_Approach_Brain_ASD_ADHD_OCD <- rbind(POND_LenVis_Brain_ASD_ADHD_OCD, POND_LenVisMet_Brain_ASD_ADHD_OCD, POND_StringVis_Brain_ASD_ADHD_OCD, POND_StringVisMet_Brain_ASD_ADHD_OCD, POND_Auto_Brain_ASD_ADHD_OCD)
 
 
+#5. Performing the interaction analysis
+HippoVol_Approach_Diag <- lm(Lhippo ~ Approach*clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_Approach_Brain_ASD_ADHD_OCD, na.action = na.omit)
+summary(HippoVol_Approach_Diag)
+HippoVol_Approach_Diag_An <- Anova(HippoVol_Approach_Diag)
+HippoVol_Approach_Diag_An
+summary(HippoVol_Approach_Diag_An)
+eta_sq(HippoVol_Approach_Diag_An)
+cohens_f(HippoVol_Approach_Diag_An)
 
 
+RHippoVol_Approach_Diag <- lm(Rhippo ~ Approach*clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_Approach_Brain_ASD_ADHD_OCD, na.action = na.omit)
+summary(RHippoVol_Approach_Diag)
+RHippoVol_Approach_Diag_An <- Anova(RHippoVol_Approach_Diag)
+RHippoVol_Approach_Diag_An
+summary(RHippoVol_Approach_Diag_An)
+cohens_f(RHippoVol_Approach_Diag_An)
+
+LmOFC_Approach_Diag <- lm(L_medialorbitofrontal_thickavg ~ Approach*clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_Approach_Brain_ASD_ADHD_OCD, na.action = na.omit)
+summary(LmOFC_Approach_Diag)
+LmOFC_Approach_Diag_An <- Anova(LmOFC_Approach_Diag)
+LmOFC_Approach_Diag_An
+cohens_f(LmOFC_Approach_Diag_An)
 
 
+RmOFC_Approach_Diag <- lm(R_medialorbitofrontal_thickavg ~ Approach*clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_Approach_Brain_ASD_ADHD_OCD, na.action = na.omit)
+summary(RmOFC_Approach_Diag)
+RmOFC_Approach_Diag_An <- Anova(RmOFC_Approach_Diag)
+RmOFC_Approach_Diag_An
+cohens_f(RmOFC_Approach_Diag_An)
 
 
+LlOFC_Approach_Diag <- lm(L_lateralorbitofrontal_thickavg ~ Approach*clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_Approach_Brain_ASD_ADHD_OCD, na.action = na.omit)
+summary(LlOFC_Approach_Diag)
+LlOFC_Approach_Diag_An <- Anova(LlOFC_Approach_Diag)
+LlOFC_Approach_Diag_An
+cohens_f(LlOFC_Approach_Diag_An)
+
+RlOFC_Approach_Diag <- lm(R_lateralorbitofrontal_thickavg ~ Approach*clin_diagnosis + age_scan + ICV.x + GENDER + scanner, data = POND_Approach_Brain_ASD_ADHD_OCD, na.action = na.omit)
+summary(RlOFC_Approach_Diag)
+RlOFC_Approach_Diag_An <- Anova(RlOFC_Approach_Diag)
+RlOFC_Approach_Diag_An
+cohens_f(RlOFC_Approach_Diag_An)
+
+
+#6. Plotting this interaction
+LHippo_Approach_POND <- function(POND_Approach_Brain_ASD_ADHD_OCD) {
+  library(ggplot2)
+  library(ggpubr)
+  library(ggsignif)
+  LHippo_Approach_POND <- ggplot(data = POND_Approach_Brain_ASD_ADHD_OCD, aes(x = Approach, y = Lhippo, fill = clin_diagnosis), na.rm = TRUE) +
+    stat_boxplot(geom = "errorbar") +
+     #stat_compare_means(method = "anova", label.y = 40) +
+    stat_compare_means(label = "p.signif", method = "t.test", ref.group = "NULL") +
+    #stat_compare_means(comparisons = list(c("Standard Visual", "Standard Visual + Metric"), c("Standard Visual", "Stringent Visual"), c("Standard Visual", "Automated"), c("Standard Visual", "Stringent Visual + Metric"))) +
+    geom_boxplot(alpha = 0.3, size = 0.2, outlier.color = 'red', outlier.shape = NA, aes(color = clin_diagnosis)) +
+    stat_summary(fun.y = 'mean', geom = 'point', shape = 8, size = 0.5) +
+    #geom_jitter(aes(color = clin_diagnosis), size = 0.05) +
+    labs(x = "Quality Control Approach", y = "Left Hippocampal Volume") +
+    theme_classic() +
+    theme(text = element_text(size= 5))
+  return(Boxplot)
+}
+
+
+tiff("LHippo_Approach_POND.tiff", width = 24, height = 20, units = "cm", res = 300)
+print(LHippo_Approach_POND)
+dev.off()
 
 
 
